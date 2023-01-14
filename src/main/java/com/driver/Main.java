7495d813e7b638dcb406c3a805a624a92f75fbac
Kannatech <sairamprasad927@gmail.com>
2023-01-14 20:01:01 +0530

    sEcond cOmmit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,23 +1,25 @@
 package com.driver;
 
-public class Main {
-    public static void main(String[] args)
+import java.io.IOException;
+
+
+    class A{
+        public String meth()
+        {
+            return "Invoking method from class A";
+        }
+    }
+    class B extends A {
+
+        public String meth() {
+            return "Method is overridden in Extendend class B";
+        }
+
+    public static void main(String[] args) throws IOException
     {
         B b=new B();
         System.out.println(b.meth());
     }
 
 }
-class A{
-    String meth()
-    {
-        return "Invoking method from class A";
-    }
-}
-class B extends A{
 
-    String meth()
-    {
-        return "Method is overridden in Extendend class B";
-    }
-}
